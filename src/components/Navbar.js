808cd0a92e081b2f9cd3d1cd45884ee8cb11f855
brycealbertazzi <40728369+brycealbertazzi@users.noreply.github.com>
2021-09-02 17:13:28 -0700

    Update to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import React from 'react'
 export const Navbar = () => {
     return (
         <div className="container mx-auto px-4 py-4">
-            <div class="flex space-x-4 ...">
-                <div class="flex-1 ...">Tasks</div>
-                <div class="flex-1 ...">Documents</div>
-                <div class="flex-1 ...">Clients</div>
+            <div className="flex space-x-4 ...">
+                <div className="flex-1 ...">Tasks</div>
+                <div className="flex-1 ...">Documents</div>
+                <div className="flex-1 ...">Clients</div>
             </div>
         </div>
     )
